773e3c7bc7c3036a18d1b4238eb1ec61faec63f4
pengdong.guo <pengdong.guo@ly.com>
2019-06-18 10:59:10 +0800

    添加博客

--- js/crash_cheat.js
+++
@@ -1,3 +0,0 @@
-// build time:Tue Jun 18 2019 10:35:51 GMT+0800 (GMT+08:00)
-var OriginTitle=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","/images/favicon-16x16-next.png");document.title="╭(°A°`)╮ 页面崩溃啦 ~";clearTimeout(titleTime)}else{$('[rel="icon"]').attr("href","/images/favicon-16x16-next.png");document.title="(ฅ>ω<*ฅ) 噫又好了~"+OriginTitle;titleTime=setTimeout(function(){document.title=OriginTitle},2e3)}});
-//rebuild by neat 
